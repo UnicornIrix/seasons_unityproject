fbd1e5c9a3ba4542ef7569c5b40240af28cc884d
UnicornIrix <iris.levrio@gmail.com>
2024-11-23 13:32:53 +0100

    Implementazione menu
    creazione delle varie schermate. creazione dei rispettivi menu, scrittura delle funzioni dei tasti.
    Da sistemare la risoluzione della camera

---
+++ esame_unity/Assets/Scripts/Menu_buttons.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_buttons : MonoBehaviour
+{
+    // Script che gestisce i bottoni della UI della schermata iniziale
+
+    public GameObject Game_menu;
+
+    // Taso Start - carica la prima scena di gioco
+    public void START_button()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Tasto Quit - esce dall'applicazione
+    public void QUIT_button()
+    {
+        Application.Quit();
+    }
+
+    // Tasto Options - apre opzioni
+    public void OPTIOINS_button()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    // Tasto Credits - apre crediti
+    public void CREDITS_button()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    // Tasto Open menu - apre il menu di gioco
+    // Attiva la ui 
+    public void OPENGM_button()
+    {
+        // SceneManager.LoadScene("Game_menu");
+
+        Game_menu.SetActive(true);
+    }
+
+    // Tasto Continue - ritorna alla main scene
+    // Disattiva la ui 
+    public void CONTINUE_button()
+    {
+        // SceneManager.LoadScene("Main");
+
+        Game_menu.SetActive(false);
+    }
+
+    // Tasto Back to menu - ritorna al menu iniziale
+    public void BACKSM_button()
+    {
+        SceneManager.LoadScene("Start_menu");
+    }
+}
