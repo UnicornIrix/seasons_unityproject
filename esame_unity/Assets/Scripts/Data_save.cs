c029e715fd0382466c04fbde76366136785ff650
stefano giordano <ste.giordano02@gmail.com>
2024-12-02 12:57:05 +0100

    Update Data_save

--- esame_unity/Assets/Scripts/Data_save.cs
+++ esame_unity/Assets/Scripts/Data_save.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using UnityEngine;
 
-public static class Data_save
+public class Data_save : MonoBehaviour
 {
     //
     // Classe che si occupa del salvataggio di dati
@@ -16,12 +16,14 @@ public static class Data_save
     // Indice della stagione in cui il player sta giocando / stava giocando prima di uscire
     private static int current_season_index = 0;
 
+    // Position of the player
+    private static Vector3 player_position = Vector3.zero;
+
 
     // Get e set
     public static void SetPROFILE(int value) {
         selected_profile_index = value;
     }
-
     public static int GetPROFILE() {
         return selected_profile_index;
     }
@@ -29,8 +31,17 @@ public static class Data_save
     public static void SetSEASON(int value) {
         current_season_index = value;
     }
-
     public static int GetSEASON() {
         return current_season_index;
+    }
+
+
+    // Salva tutto
+    public static void SaveALL() {
+        PlayerPrefs.SetFloat("Xpos" + selected_profile_index, player_position.x);
+        PlayerPrefs.SetFloat("Ypos" + selected_profile_index, player_position.y);
+        PlayerPrefs.SetFloat("Zpos" + selected_profile_index, player_position.z);
+
+        PlayerPrefs.SetInt("Season" + selected_profile_index, current_season_index);
     }
 }
