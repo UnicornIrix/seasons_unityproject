bb964b20d1a9a2fe6e1c4b124ba6b75235310f8e
UnicornIrix <iris.levrio@gmail.com>
2025-01-09 13:35:16 +0100

    modifiche da pc di iris
    (risolto nulla)

--- esame_unity/Assets/Prefabs/Natura/prova_cambio_stagioni.cs
+++ esame_unity/Assets/Prefabs/Natura/prova_cambio_stagioni.cs
@@ -7,21 +7,33 @@ public class prova_cambio_stagioni : MonoBehaviour
     public Animator animator;
 
     public void SET_Estate() {
-        animator.Play("Estate");
+        animator.SetBool("bool_est", true);
+        animator.SetBool("bool_aut", false);
+        animator.SetBool("bool_inv", false);
+        animator.SetBool("bool_prim", false);
     }
 
     public void SET_Autunno()
     {
-        animator.Play("Autunno");
+        animator.SetBool("bool_est", false);
+        animator.SetBool("bool_aut", true);
+        animator.SetBool("bool_inv", false);
+        animator.SetBool("bool_prim", false);
     }
 
     public void SET_Inverno()
     {
-        animator.Play("Inverno");
+        animator.SetBool("bool_est", false);
+        animator.SetBool("bool_aut", false);
+        animator.SetBool("bool_inv", true);
+        animator.SetBool("bool_prim", false);
     }
 
     public void SET_Primavera()
     {
-        animator.Play("Primavera");
+        animator.SetBool("bool_est", false);
+        animator.SetBool("bool_aut", false);
+        animator.SetBool("bool_inv", false);
+        animator.SetBool("bool_prim", true);
     }
 }
