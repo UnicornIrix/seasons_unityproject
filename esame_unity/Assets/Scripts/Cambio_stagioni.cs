bf83fd112c4cf556dbc74831ef07e3d2c59192a4
UnicornIrix <iris.levrio@gmail.com>
2025-01-20 01:05:14 +0100

    basta

--- esame_unity/Assets/Scripts/Cambio_stagioni.cs
+++ esame_unity/Assets/Scripts/Cambio_stagioni.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cambio_stagioni : MonoBehaviour
@@ -12,9 +13,9 @@ public class Cambio_stagioni : MonoBehaviour
     [SerializeField]
     private ParticleSystem PS_neve;
 
-    private Animator animator;
-
     private ParticleSystem PS_foglie;
+
+    private Animator animator;
 
     [SerializeField]
     private Material blue_sky;
@@ -22,15 +23,21 @@ public class Cambio_stagioni : MonoBehaviour
     [SerializeField]
     private Material grey_sky;
 
+    private string animation_name;
+
     private void Start()
     {
         alberi = GameObject.FindGameObjectsWithTag("Albero");
     }
 
 
-    public void TRANSITION_Primavera() {
-        if (Data_save.current_season_index != 1) { 
+    public void TRANSITION_Primavera()
+    {
+        if (Data_save.current_season_index != 1)
+        {
             Data_save.current_season_index = 1;
+
+            // animation_name = "to_primavera";
 
             start_animation("to_primavera", false, alberi);
 
@@ -48,8 +55,11 @@ public class Cambio_stagioni : MonoBehaviour
 
     public void TRANSITION_Estate()
     {
-        if (Data_save.current_season_index != 2) { 
+        if (Data_save.current_season_index != 2)
+        {
             Data_save.current_season_index = 2;
+
+            // animation_name = "to_estate";
 
             start_animation("to_estate", false, alberi);
 
@@ -66,8 +76,11 @@ public class Cambio_stagioni : MonoBehaviour
 
     public void TRANSITION_Autunno()
     {
-        if (Data_save.current_season_index != 3) { 
+        if (Data_save.current_season_index != 3)
+        {
             Data_save.current_season_index = 3;
+
+            //animation_name = "to_autunno";
 
             start_animation("to_autunno", true, alberi);
 
@@ -83,8 +96,11 @@ public class Cambio_stagioni : MonoBehaviour
 
     public void TRANSITION_Inverno()
     {
-        if (Data_save.current_season_index != 4) { 
+        if (Data_save.current_season_index != 4)
+        {
             Data_save.current_season_index = 4;
+
+            // animation_name = "to_inverno";
 
             start_animation("to_inverno", false, alberi);
 
@@ -100,12 +116,15 @@ public class Cambio_stagioni : MonoBehaviour
     }
 
     // starta sia l'animazione degli alberi che il particellare delle foglie
-    public void start_animation(string animation_name, bool isAutunno, GameObject[] gameObjects) {
+    public void start_animation(string animation_name, bool isAutunno, GameObject[] gameObjects)
+    {
         foreach (GameObject go in gameObjects)
         {
             animator = go.GetComponent<Animator>();
             PS_foglie = go.GetComponentInChildren<ParticleSystem>();
+
             animator.SetTrigger(animation_name);
+
             PS_foglie.enableEmission = isAutunno;
         }
     }
--- esame_unity/Assets/Scripts/Data_load.cs
+++ esame_unity/Assets/Scripts/Data_load.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Data_load : MonoBehaviour
 {
-    public GameObject player_gameobject;
+    private GameObject player_gameobject;
+    private Cambio_stagioni cambio_Stagioni;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Data_save.LoadALL();
+        Debug.Log(Data_save.selected_profile_index);
+        Data_save.LoadALL();
+        player_gameobject = GameObject.FindGameObjectWithTag("Player");
+        cambio_Stagioni = this.GetComponent<Cambio_stagioni>();
 
-        player_gameobject.transform.position = Data_save.player_position;
+        //new Vector3(736, 14.98f, 398);
+        
+
+       // player_gameobject.transform.position = Data_save.player_position;
+        //PlayerPrefs.GetFloat("Xpos" + Data_save.selected_profile_index);
+
+        switch (PlayerPrefs.GetInt("Season" + PlayerPrefs.GetInt("Slot")))
+        {
+            case 1:
+                cambio_Stagioni.TRANSITION_Primavera();
+                break;
+            case 3:
+                cambio_Stagioni.TRANSITION_Autunno();
+                break;
+            case 4:
+                cambio_Stagioni.TRANSITION_Inverno();
+                break;
+            default:
+                break;
+        }
     }
 }
